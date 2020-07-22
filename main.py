8a5886d4c51b09dfd17d55e9e58ad086a2d8727d
xiaodao <826359422@qq.com>
2020-07-22 09:54:28 +0800

    update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from trainer.trainer_factory import TrainerFactory
 sys.path.append('.')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--exp_dir', type=str, default=r'E:\experimental\deblur\dccan_share_pytorch_model\two_level_plus_with_skip\mscan', help='the directory of experiment')
-parser.add_argument('--input_path', type=str, default=r'E:\experimental\deblur\code_and_data\Gopro', help='real image path')
-parser.add_argument('--output_path', type=str, default=r'E:\experimental\deblur\dccan_share_pytorch_model\two_level_plus_with_skip\mscan', help='test result output path')
+parser.add_argument('--exp_dir', type=str, default='.', help='the directory of experiment')
+parser.add_argument('--input_path', type=str, default='.', help='real image path')
+parser.add_argument('--output_path', type=str, default='.', help='test result output path')
 parser.add_argument('--save_model_name', type=str, default='mscan', help='the name of the saved model')
 parser.add_argument('--trainer_name', type=str, default='MSCAN',
                     choices=['MSCAN'], help='optional model')
